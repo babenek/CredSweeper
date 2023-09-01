09f8128f0734d9fe5839a193cf7bc338f292d838
Roman Babenko <babenek@gmail.com>
2023-09-01 07:04:55 +0300

    ip_id_passwd_triple

--- tests/test_main.py
+++ tests/test_main.py
@@ -729,6 +729,15 @@ class TestMain(unittest.TestCase):
 
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
+    def test_ip_id_passwd_triple_p(self) -> None:
+        content_provider: FilesProvider = TextProvider([SAMPLES_PATH / "ip_id_password_triple"])
+        cred_sweeper = CredSweeper(doc=True, severity=Severity.CRITICAL)
+        cred_sweeper.run(content_provider=content_provider)
+        found_credentials = cred_sweeper.credential_manager.get_credentials()
+        self.assertEqual(5, len(found_credentials))
+
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
     def test_data_p(self) -> None:
 
         def prepare(report: List[Dict[str, Any]]):
