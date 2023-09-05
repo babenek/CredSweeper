fc2387197245e15b49e97d6184e20447e1d45060
Roman Babenko <babenek@gmail.com>
2023-09-05 23:10:57 +0300

    tmp

--- tests/test_doc.py
+++ tests/test_doc.py
@@ -10,21 +10,23 @@ from tests import SAMPLES_PATH
 
 
 class TestDoc(unittest.TestCase):
+    def setUp(self) -> None:
+        self.cred_sweeper = CredSweeper(doc=True, severity=Severity.CRITICAL, ml_threshold=0)
+
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
     def test_ip_id_passwd_triple_p(self) -> None:
         content_provider: FilesProvider = TextProvider([SAMPLES_PATH / "doc_ip_id_password_triple"])
-        cred_sweeper = CredSweeper(doc=True, severity=Severity.CRITICAL)
-        cred_sweeper.run(content_provider=content_provider)
-        found_credentials = cred_sweeper.credential_manager.get_credentials()
+        self.cred_sweeper.run(content_provider=content_provider)
+        found_credentials = self.cred_sweeper.credential_manager.get_credentials()
         self.assertEqual(5, len(found_credentials))
 
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
     def test_passwd_pair_p(self) -> None:
         content_provider: FilesProvider = TextProvider([SAMPLES_PATH / "doc_id_password_pair"])
-        cred_sweeper = CredSweeper(doc=True, severity=Severity.CRITICAL)
-        cred_sweeper.run(content_provider=content_provider)
-        found_credentials = cred_sweeper.credential_manager.get_credentials()
+        self.cred_sweeper.run(content_provider=content_provider)
+        found_credentials = self.cred_sweeper.credential_manager.get_credentials()
         self.assertEqual(15, len(found_credentials), found_credentials)
 
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
