598d20636ca0ea33cb310697169ad27f437ec8f7
Roman Babenko <babenek@gmail.com>
2026-01-30 09:28:56 +0200

    [skip actions] [litert] 2026-01-30T09:28:56+02:00

--- start.sh
+++ start.sh
@@ -7,9 +7,9 @@ export JOBS=16
 export CREDSWEEPER_DIR=/home/babenek/q/CredSweeper/litert
 export CREDDATA_DIR=/home/babenek/q/DataCred/quick
 
-export PYTHONPATH=/home/babenek/q/CredSweeper/pytorch_migration:"${PYTHONPATH}"
+export PYTHONPATH=/home/babenek/q/CredSweeper/litert:"${PYTHONPATH}"
 
 bash experiment/main.sh | tee $(date +%Y%m%d_%H%M%S).log
 
 # check train
-.venv/bin/python -m credsweeper --path ./tests/samples/ --color --no-stdout
+python -m credsweeper --path ./tests/samples/ --color --no-stdout
